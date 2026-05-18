107929f23009d8293f5b9713e093c338478f7487
OpenCow <opencow@ik.me>
2026-05-18 11:35:01 +0200

    fix: order mb formula metadata

--- Formula/mb.rb
+++ Formula/mb.rb
@@ -1,7 +1,6 @@
 class Mb < Formula
   desc "Local document memory CLI for agents and tools"
   homepage "https://github.com/OpenCow42/metaBrain"
-  license "BSD-3-Clause"
 
   github_token = ENV["HOMEBREW_GITHUB_API_TOKEN"] || ENV["GITHUB_TOKEN"]
   github_auth_headers = github_token.to_s.empty? ? [] : ["Authorization: Bearer #{github_token}"]
@@ -10,6 +9,7 @@ class Mb < Formula
       headers: github_auth_headers + ["Accept: application/octet-stream"]
   sha256 "4000c0f97756ba528a06a2c830b138720dff9f4e06e40dc65a8ae2aaf84d2400"
   version "1.0.0"
+  license "BSD-3-Clause"
 
   def install
     bin.install "bin/mb"
