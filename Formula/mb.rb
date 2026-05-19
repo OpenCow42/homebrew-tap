a19f18e3ca21eba1d47168816b65d5e6906180e8
OpenCow <opencow@ik.me>
2026-05-19 13:00:34 +0200

    mb 1.1.0

--- Formula/mb.rb
+++ Formula/mb.rb
@@ -5,10 +5,10 @@ class Mb < Formula
   github_token = ENV["HOMEBREW_GITHUB_API_TOKEN"] || ENV["GITHUB_TOKEN"]
   github_auth_headers = github_token.to_s.empty? ? [] : ["Authorization: Bearer #{github_token}"]
 
-  url "https://api.github.com/repos/OpenCow42/metaBrain/releases/assets/423161822",
+  url "https://api.github.com/repos/OpenCow42/metaBrain/releases/assets/424119154",
       headers: github_auth_headers + ["Accept: application/octet-stream"]
-  version "1.0.0"
-  sha256 "4000c0f97756ba528a06a2c830b138720dff9f4e06e40dc65a8ae2aaf84d2400"
+  version "1.1.0"
+  sha256 "e24ecae9a852e982a1ca354796daad84d71c60eb55427c09a15270455b759609"
   license "BSD-3-Clause"
 
   def install
