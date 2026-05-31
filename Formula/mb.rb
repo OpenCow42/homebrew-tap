235c1640bbb10dc579bf61b92f65c35403b0ca6b
OpenCow <opencow@ik.me>
2026-05-31 10:19:50 +0200

    brew: update mb to 1.2.1

--- Formula/mb.rb
+++ Formula/mb.rb
@@ -2,8 +2,8 @@ class Mb < Formula
   desc "Local document memory CLI for agents and tools"
   homepage "https://github.com/OpenCow42/metaBrain"
 
-  url "https://github.com/OpenCow42/metaBrain/releases/download/1.2.0/mb-1.2.0-macos-universal.zip"
-  sha256 "4112bb5ec99cc27d6627a20e1b6c636d01af356d7ea79d815ca77aaf308d7083"
+  url "https://github.com/OpenCow42/metaBrain/releases/download/1.2.1/mb-1.2.1-macos-universal.zip"
+  sha256 "388c0d3fd5bc5fea17630d37011c9780dda3d855a40ba18674dbfdb2b1080f88"
   license "BSD-3-Clause"
 
   def install
