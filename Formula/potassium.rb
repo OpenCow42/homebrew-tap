f7685e3501cda4c169982b076b63593ee4641576
OpenCow <opencow@ik.me>
2026-06-11 12:24:13 +0200

    brew: update potassium to 0.0.3

--- Formula/potassium.rb
+++ Formula/potassium.rb
@@ -1,8 +1,8 @@
 class Potassium < Formula
   desc "Infomaniak suite command-line client"
   homepage "https://github.com/OpenCow42/tool-releases"
-  url "https://github.com/OpenCow42/tool-releases/releases/download/potassium-0.0.2/potassium-0.0.2-macos-universal.zip"
-  sha256 "447a1df84ab3d795c54e537ccfe6c5ccfff0c89e284ca1c353bea80023c49f79"
+  url "https://github.com/OpenCow42/tool-releases/releases/download/potassium-0.0.3/potassium-0.0.3-macos-universal.zip"
+  sha256 "d1955a6065881b90c11a656a10c4e0379e5661405bcae1bee515595e9bf592f5"
   license :cannot_represent
 
   def install
@@ -12,7 +12,7 @@ class Potassium < Formula
   end
 
   test do
-    assert_equal "0.0.2", shell_output("#{bin}/pot version").strip
+    assert_equal "0.0.3", shell_output("#{bin}/pot version").strip
     assert_match "Potassium", shell_output("#{bin}/pot --help")
   end
 end
